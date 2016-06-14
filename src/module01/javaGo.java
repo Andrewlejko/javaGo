561bc4d00e28b966696759c8c40e58079cdf937a
Andrewlejko <lejko579@gmail.com>
2016-06-14 21:15:05 +0300

    Initial commit

---
+++ src/module01/javaGo.java
@@ -0,0 +1,10 @@
+package module01;
+
+/**
+ * Created by Michael on 13.06.2016.
+ */
+public class javaGo {
+    public static void main(String[]args){
+        System.out.println("Hi, I\'m your first Java program! Stay tuned");
+    }
+}
